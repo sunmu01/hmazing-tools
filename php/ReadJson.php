a7f9e8d5592906c5dae3817012317e0782c0eb0e
sunday <1114211821@qq.com>
2017-11-29 10:27:58 +0800

    vendor manage

--- php/ReadJson.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../Json.php';
-
-use Desarrolla2\File\Json;
-
-$json = Json::read('/www/html/trees/vendors.json', $json);
-
-return $json;
--- src/components/VendorMap.vue
+++ src/components/VendorMap.vue
@@ -6,32 +6,45 @@
 				<thead>
 					<tr>
 						<th>Title</th>
-						<th>Logo</th>
-						<th>Description</th>
 						<th>Products Count</th>
-						<th>Edit</th>
+						<th>Description</th>
+						<th>Logo</th>
+						<th>#</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr v-for="(v, i) in vendors">
 						<td>{{ v.title }}</td>
-						<td>
-							<input class="input" type="text" v-model="v.logo" v-if="v.editing">
-							<span v-else>{{ v.logo }}</span>
-						</td>
-						<td>
-							<input class="input" type="text" v-model="v.desc" v-if="v.editing">
-							<span v-else>{{ v.desc }}</span>
-						</td>
 						<td>{{ v.count }}</td>
-						<td>
-							<a @click="editData(i)">
-								<span v-if="v.editing">Save</span><span v-else>Edit</span>
-							</a>
-						</td>
+						<td><span>{{ v.desc }}</span></td>
+						<td><img class="w1" :src="v.image"></td>
+						<td><a class="btn" @click="editData(v)">Edit</a></td>
 					</tr>
 				</tbody>
 			</table>
+		</div>
+
+		<div class="clearfix fixed top-0 bottom-0 left-0 right-0 border-box p2 flex items-center justify-center flex-wrap content-center bg-darken-4 z1" v-show="modaling">
+			<div class="col-12 max-width-3 p2 bg-white rounded">
+				<div class="col col-12 border-box p1 flex items-start justify-start">
+					<label class="label col-2">Title:</label>
+					<span class="col-10">{{ item.title }}</span>
+				</div>
+				<div class="col col-12 border-box p1 flex items-start justify-start">
+					<label class="label col-2">Description:</label>
+					<textarea rows="5" class="textarea m0 col-10" v-model="item.desc"></textarea>
+				</div>
+				<div class="col col-12 border-box p1 flex items-start justify-start">
+					<label class="label col-2">Logo:</label>
+					<textarea class="textarea m0 col-10" v-model="item.image"></textarea>
+				</div>
+				<div class="col col-12 border-box p1 center">
+					<img class="max-h2 p1" :src="item.image">
+				</div>
+				<div class="col col-12 border-box p1 center">
+					<a class="btn btn-primary px4" @click="saveData(item)">Save</a>
+				</div>
+			</div>
 		</div>
 	</layout>
 </template>
@@ -42,7 +55,16 @@ export default {
 	name: 'VendorMap',
 	data () {
 		return {
-			vendors: []
+			api: 'http://11.11.11.11:9000/',
+			vendors: [],
+			modaling: false,
+			item: {
+				id: '',
+				title: '',
+				image: '',
+				desc: '',
+				count: 0
+			}
 		}
 	},
 	created () {
@@ -51,45 +73,50 @@ export default {
 		})
 	},
 	methods: {
-		editData (i) {
+		editData (v) {
 			let self = this
-			self.vendors[i].editing = !self.vendors[i].editing
-			if (!self.vendors[i].editing) {
-				self.saveData()
-			}
+			self.modaling = true
+			self.item = v
 		},
-		saveData () {
-			var data = JSON.stringify(self.vendors)
-			var params = { method: 'post', body: data }
-			fetch('https://sea.hmazing.com/SaveJson.php', params)
-			.then((response) => {
-				return response.json()
-			}).then((data) => {
-				console.log(data)
+		saveData (v) {
+			let self = this
+			let image = v.image == '' ? null : v.image
+			let desc = v.desc == '' ? null : v.desc
+			let data = JSON.stringify({
+				"fields": {
+					"image": { "assign": image },
+					"description": { "assign": desc }
+				}
 			})
-		},
-		readData () {
-			fetch('https://sea.hmazing.com/ReadJson.php')
-			.then((response) => {
+			let params = {
+				method: 'put',
+				body: data
+			}
+			let url = self.api + 'document/v1/hmazing/vendor/docid/' + v.id
+			fetch(url, params).then((response) => {
 				return response.json()
 			}).then((data) => {
 				console.log(data)
-				self.vendors = data
+				self.modaling = false
 			})
 		},
 		fetchData () {
 			let self = this
-			let uri = 'https://sea.hmazing.com/search/?hits=0&yql=select%20*from%20sources%20product%20where%20price_min>-1|all(group(vendor)each(output(count())));'
-			let fet = fetch(uri).then(self.checkStatus).then((response) => {
-				let arr = response.root.children[0].children[0].children
+			let url = self.api + 'search/?yql=select%20*from%20sources%20vendor%20where%20products_count%3E-1;'
+			let fet = fetch(url).then(self.checkStatus).then((response) => {
+				let arr = response.root.children
 				if (arr) {
 					for (var i in arr) {
+						let image = arr[i].fields.image
+						image = image == 'null' ? '' : image
+						let desc = arr[i].fields.description
+						desc = desc == 'null' ? '' : desc
 						self.vendors.push({
-							title: arr[i].value,
-							logo: '',
-							desc: '',
-							count: arr[i].fields['count()'],
-							editing: false
+							id: arr[i].fields.id,
+							title: arr[i].fields.title,
+							image: image,
+							desc: desc,
+							count: arr[i].fields.products_count
 						})
 					}
 				}
@@ -118,20 +145,26 @@ export default {
 
 <style scoped>
 table {
-  width: 100%;
-  max-width: 100%;
-  border-spacing: 0;
-  border-collapse: collapse;
-  background-color: transparent;
+	width: 100%;
+	max-width: 100%;
+	border-spacing: 0;
+	border-collapse: collapse;
+	background-color: transparent;
 }
 table > thead {
 	background-color: #e5e5e5;
 }
 td, th {
-  padding: .5rem;
-  border-top: 1px solid #e5e5e5;
+	padding: .5rem;
+	border-top: 1px solid #e5e5e5;
 }
 table > tbody > tr:hover {
-  background-color: #f5f5f5;
+	background-color: #f5f5f5;
+}
+.w1 {
+	max-width: 100px;
+}
+.max-h2 {
+	max-height: 200px;
 }
 </style>
