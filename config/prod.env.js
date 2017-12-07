045e5c2482029dd49281805b4635ac6f87da9f4c
sunday <1114211821@qq.com>
2017-12-07 11:52:51 +0800

    for aliyun deploy

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // API_URL: '"https://sea.hmazing.com/"'
-  API_URL: '"http://11.11.11.11:9000/"'
+  API_URL: '"https://sea.hmazing.com/"'
+  // API_URL: '"http://11.11.11.11:9000/"'
 }
